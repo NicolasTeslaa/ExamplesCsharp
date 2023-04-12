57ac0b01e37cc1da7cf2d21b5ba732630cb715a0
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-12 17:44:49 -0300

    formatting date

--- DateTimes/FormattingDate.cs
+++ DateTimes/FormattingDate.cs
@@ -12,6 +12,20 @@ namespace DateTimes
             Console.WriteLine(formatada);
 
             // z signfica time zone onde o sistema está configurado
+
+            //temos tambem algumas formatações predefinidas 
+            //formatação padrão do tempo
+            var timeFormatter = String.Format("{0:t}", dataAtual);
+            Console.WriteLine(timeFormatter);
+            //formatação padrão do tempo em extenso
+            var timeFormatterExtenser = String.Format("{0:T}", dataAtual);
+            Console.WriteLine(timeFormatterExtenser);
+            //formatação padrão da data em extenso
+            var dateFormatter = String.Format("{0:D}", dataAtual);
+            Console.WriteLine(dateFormatter);
+            //formatação padrão da data minima
+            var dateFormatterMinimal = String.Format("{0:d}", dataAtual);
+            Console.WriteLine(dateFormatterMinimal);
         }
     }
 }
