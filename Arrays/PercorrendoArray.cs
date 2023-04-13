448b6b0acf5d1865a9cdb809b879e0400817b6aa
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-13 14:17:13 -0300

    implementando for each

---
+++ Arrays/PercorrendoArray.cs
@@ -0,0 +1,30 @@
+namespace Arrays
+{
+    public class PercorrendoArray
+    {
+        public static void Metodo()
+        {
+            var meuArray = new int[5] { 1, 2, 3, 4, 5 };
+
+            //mostra o tipo, no caso = System.Int32[]
+            Console.WriteLine(meuArray);
+
+            //mostra a quantidade de itens dentro do array
+            Console.WriteLine(meuArray.Length);
+
+            //mostra o conteudo em cada i dentro do array
+            for (int i = 0; i < meuArray.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //percorre o conteudo de cada um dentro do array porém não precisamos especificar tantas coisas
+            // pra cada item do meu array
+            foreach (var item in meuArray)
+            {
+                //faça tal coisa
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,5 +1,3 @@
-﻿Console.Clear();
+﻿using Arrays;
 
-var array = new string[5] { "0", "1", "2", "3", "4" };
-
-Console.WriteLine(array[2]);
+PercorrendoArray.Metodo();
