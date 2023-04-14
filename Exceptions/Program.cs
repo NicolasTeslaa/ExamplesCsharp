6664b8ceb8bcd140e93f46f7983e893846d53f78
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-04-14 13:39:49 -0300

    atts

---
+++ Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿//exemplo de exceção
+var arr = new int[3];
+
+
+//tenta executar o for
+//dentro do try é o ideal colocar apenas os métodos principais e não declarar nenhum tipo de variavel
+try
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
+// se der errado faça tal coisa
+catch
+{
+    Console.WriteLine("Deu ruim menó");
+}
