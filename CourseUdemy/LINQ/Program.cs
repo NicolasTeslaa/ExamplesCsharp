ef6dde44e2b7521903914e8be70e0b9e90ee62f9
Nicolas Tesla <65693466+NicolasTeslaa@users.noreply.github.com>
2023-05-17 16:58:08 -0300

    atts

--- CourseUdemy/LINQ/Program.cs
+++ CourseUdemy/LINQ/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿List<string> nomes = new List<string>()
+        {
+        "ana", "nicolas", "lucas", "carla","alberto"
+        };
+
+// primeiro exemplo extenso
+var resultado = from m in nomes
+                where m.Contains('o')
+                select m;
+
+foreach(var item in resultado)
+        Console.WriteLine(item);
+
+
+//segundo exemplo mais compacto
